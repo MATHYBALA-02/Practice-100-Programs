23d521fccbb85af7b605ebf64a882b173018ed17
MATHYBALA-02 <112637571+MATHYBALA-02@users.noreply.github.com>
2023-02-03 00:07:51 +0530

    Create hsubtb.v

---
+++ Combinational/Subtractor/Half Subtractor/hsubtb.v
@@ -0,0 +1,17 @@
+module hsub_tb;
+  reg a,b;
+  wire diff,borrow;
+  hsub HSUB(diff,borrow,a,b);
+  initial 
+    begin
+      $dumpfile("dump.vcd");
+      $dumpvars(1);
+    end
+  initial
+    begin
+      a=0;b=0;
+      #5; a=0;b=1;
+      #5; a=1;b=0;
+      #5;a=1;b=1;
+    end
+endmodule
