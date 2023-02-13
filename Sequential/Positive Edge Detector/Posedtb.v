caf88b5ebeea1cc7fad70a8996c92869597f1e55
MATHYBALA-02 <112637571+MATHYBALA-02@users.noreply.github.com>
2023-02-13 22:03:39 +0530

    Create Posedtb.v

---
+++ Sequential/Positive Edge Detector/Posedtb.v
@@ -0,0 +1,20 @@
+module posedgedetecttb;
+	reg sig;          	
+	reg clk;                                
+	wire pe;
+	pos_edgedetect  uut (.sig(sig), .clk(clk),.pe(pe));
+	always #5 clk = ~clk;           
+	initial begin
+		clk <= 0;
+		sig <= 0;
+		#15 sig <= 1;
+		#20 sig <= 0;
+		#15 sig <= 1;
+		#10 sig <= 0;
+		#20 $finish;
+	end	
+  	initial begin
+      	$dumpvars;
+      $dumpfile("dump.vcd");
+    end
+endmodule
