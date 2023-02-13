8dbe2f8a5702acd56f487793ab29a3b199f35922
MATHYBALA-02 <112637571+MATHYBALA-02@users.noreply.github.com>
2023-02-13 21:26:35 +0530

    Create Dlgatetb.v

---
+++ Sequential/D Latchgate/Dlgatetb.v
@@ -0,0 +1,22 @@
+module d_latch_gate_tb;
+  reg clk, in;
+  wire out;
+
+  d_latch_gate dut (clk, in, out);
+initial
+    begin
+      $dumpfile("dump.vcd");
+      $dumpvars();
+    end
+  initial begin
+    clk = 0; in = 0;
+    #5 clk = 1; in = 1;
+    #5 clk = 0;in=0;
+    #5 clk = 1;in=1;
+    #5 clk = 0;in=0;
+    #5 $finish;
+  end
+
+  always #5 clk = ~clk;
+
+endmodule
